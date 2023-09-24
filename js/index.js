ab526800cd01876f53dfec561b3a090ff037a98f
fedororova <al.fedorova02@gmail.com>
2023-09-24 17:13:56 +0300

    header

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const menuBtn = document.querySelector(".menu-btn");
+const menuLinks = document.querySelectorAll(".menu__list-link");
+const menuList = document.querySelector(".menu__list");
+
+menuBtn.addEventListener("click", function () {
+    menuList.classList.toggle("menu__list--active");
+    menuBtn.classList.toggle("active");
+});
+
+menuLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+        menuList.classList.remove("menu__list--active");
+        menuBtn.classList.remove("active");
+    });
+});
